877ebaff7ba7b0f0ce44e92d785075647545e777
kevinlemonra <kevinlemonra@gmail.com>
2022-01-27 04:48:09 +0800

    Add behavior test mode. (#42)
    ./go-stun -b -s stun.stunprotocol.org:3478
    Mapping Behavior: EndpointIndependent
    Filtering Behavior: AddressAndPortDependent
    Normal NAT Type: Port Restricted cone NAT

--- stun/discover.go
+++ stun/discover.go
@@ -164,3 +164,90 @@ func (c *Client) discover(conn net.PacketConn, addr *net.UDPAddr) (NATType, *Hos
 	}
 	return NATSymmetric, mappedAddr, nil
 }
+
+func (c *Client) behaviorTest(conn net.PacketConn, addr *net.UDPAddr) (*NATBehavior, error) {
+	natBehavior := &NATBehavior{}
+
+	// Test1   ->(IP1,port1)
+	// Perform test to check if it is under NAT.
+	c.logger.Debugln("Do Test1")
+	resp1, err := c.test(conn, addr)
+	if err != nil {
+		return nil, err
+	}
+	// identical used to check if it is open Internet or not.
+	if resp1.identical {
+		return nil, errors.New("Not behind a NAT.")
+	}
+	// use otherAddr or changedAddr
+	otherAddr := resp1.otherAddr
+	if otherAddr == nil {
+		if resp1.changedAddr != nil {
+			otherAddr = resp1.changedAddr
+		} else {
+			return nil, errors.New("Server error: no other address and changed address.")
+		}
+	}
+
+	// Test2   ->(IP2,port1)
+	// Perform test to see if mapping to the same IP and port when
+	// send to another IP.
+	c.logger.Debugln("Do Test2")
+	tmpAddr := &net.UDPAddr{IP: net.ParseIP(otherAddr.IP()), Port: addr.Port}
+	resp2, err := c.test(conn, tmpAddr)
+	if err != nil {
+		return nil, err
+	}
+	if resp2.mappedAddr.IP() == resp1.mappedAddr.IP() &&
+		resp2.mappedAddr.Port() == resp1.mappedAddr.Port() {
+		natBehavior.MappingType = BehaviorTypeEndpoint
+	}
+
+	// Test3   ->(IP2,port2)
+	// Perform test to see if mapping to the same IP and port when
+	// send to another port.
+	if natBehavior.MappingType == BehaviorTypeUnknown {
+		c.logger.Debugln("Do Test3")
+		tmpAddr.Port = int(otherAddr.Port())
+		resp3, err := c.test(conn, tmpAddr)
+		if err != nil {
+			return nil, err
+		}
+		if resp3.mappedAddr.IP() == resp2.mappedAddr.IP() &&
+			resp3.mappedAddr.Port() == resp2.mappedAddr.Port() {
+			natBehavior.MappingType = BehaviorTypeAddr
+		} else {
+			natBehavior.MappingType = BehaviorTypeAddrAndPort
+		}
+	}
+
+	// Test4   ->(IP1,port1)   (IP2,port2)->
+	// Perform test to see if the client can receive packet sent from
+	// another IP and port.
+	c.logger.Debugln("Do Test4")
+	resp4, err := c.testChangeBoth(conn, addr)
+	if err != nil {
+		return natBehavior, err
+	}
+	if resp4 != nil {
+		natBehavior.FilteringType = BehaviorTypeEndpoint
+	}
+
+	// Test5   ->(IP1,port1)   (IP1,port2)->
+	// Perform test to see if the client can receive packet sent from
+	// another port.
+	if natBehavior.FilteringType == BehaviorTypeUnknown {
+		c.logger.Debugln("Do Test5")
+		resp5, err := c.testChangePort(conn, addr)
+		if err != nil {
+			return natBehavior, err
+		}
+		if resp5 != nil {
+			natBehavior.FilteringType = BehaviorTypeAddr
+		} else {
+			natBehavior.FilteringType = BehaviorTypeAddrAndPort
+		}
+	}
+
+	return natBehavior, nil
+}
